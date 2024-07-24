4f817f6502bc2feac47c50e6fd397e0b1986cdb4
Tobiasz Tonn <toneq@tlen.pl>
2024-07-24 22:37:48 +0200

    dodanie migracji

--- database/migrations/2024_07_17_123949_create_access_token_table.php
+++ database/migrations/2024_07_17_123949_create_access_token_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('token');
             $table->string('service');
+            $table->string('type');
             $table->timestamps();
         });
     }
