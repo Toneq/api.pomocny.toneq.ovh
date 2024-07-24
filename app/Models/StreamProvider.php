2c27a88b4cf266eff4f91860b12cf26569624f95
Tobiasz Tonn <toneq@tlen.pl>
2024-07-24 23:19:14 +0200

    xxx

--- app/Models/StreamProvider.php
+++ app/Models/StreamProvider.php
@@ -10,4 +10,11 @@ class StreamProvider extends Model
     use HasFactory;
 
     protected $table = 'stream_providers';
+
+    protected $fillable = [
+        'user_id',
+        'service',
+        'user_provider_id',
+        'active'
+    ];
 }
