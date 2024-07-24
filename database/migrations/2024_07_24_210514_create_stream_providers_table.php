6f04f79a7a79c281e50ec1ec36657ee5689781a3
Tobiasz Tonn <toneq@tlen.pl>
2024-07-24 23:08:46 +0200

    x

--- database/migrations/2024_07_24_210514_create_stream_providers_table.php
+++ database/migrations/2024_07_24_210514_create_stream_providers_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('stream_providers', function (Blueprint $table) {
             $table->id();
+            $table->integer('user_id');
+            $table->integer('user_provider_id');
+            $table->integer('active');
             $table->timestamps();
         });
     }
